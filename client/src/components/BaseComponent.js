22a62c255ee572e6575d1ea6e760c35754a328a4
ivan3123708 <ivan3123708@gmail.com>
2018-04-24 00:11:55 +0200

    Added boilerplate front-end

---
+++ client/src/components/BaseComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BaseComponent = () => (
+  <div>
+    <h1>HOME PAGE</h1>
+  </div>
+);
+
+export default BaseComponent;
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BaseComponent from './components/BaseComponent';
+
+ReactDOM.render(<BaseComponent />, document.getElementById('app_root'));
